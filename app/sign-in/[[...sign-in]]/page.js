3b69a9f4254d996f9ad97b81c2006bb0c0de1703
VedantCars2004 <vgohe2@illinois.edu>
2024-08-18 01:30:16 -0500

    added redirections for sign in/ sign up pages where necessary, updated ui on both those pages

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,34 +1,59 @@
-import { AppBar, Container, Button, Typography, Toolbar } from "@mui/material";
+'use client'
+import { AppBar, Container, Button, Typography, Toolbar, Paper, useTheme, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
 import { SignIn } from '@clerk/nextjs';
 import Link from 'next/link';
 
 export default function SignInPage() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <Container maxWidth="lg">
-            <AppBar position="static">
+        <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
+            <AppBar position="static" elevation={4} sx={{ bgcolor: 'primary.main' }}>
                 <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        Sign In
+                    <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+                        Flashcard SaaS
                     </Typography>
-                   
-                    <Button color="inherit" component={Link} href="/">
+                    <Button 
+                        color="inherit" 
+                        component={Link} 
+                        href="/"
+                        sx={{ 
+                            '&:hover': { bgcolor: 'primary.dark' }
+                        }}
+                    >
                         Home
                     </Button>
                 </Toolbar>
             </AppBar>
-            <br />
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    minHeight: '80vh',
-                }}
-            >
-                <SignIn redirectUrl="/flashcards" />
-            </Box>
-        </Container>
+            <Typography variant="body2" sx={{ marginTop: 2 }} align="center" alignItems={'center'}>
+                    Don't have an account?{' '}
+                    <Link href="/sign-up" passHref>
+                        <Button variant="text">Sign Up</Button>
+                    </Link>
+                </Typography>
+            <Container maxWidth="sm" sx={{ mt: isMobile ? 4 : 8, display: 'flex', justifyContent: 'center', alignItems: 'center', flexGrow: 1 }}>
+                <Paper 
+                    elevation={3} 
+                    sx={{
+                        p: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        borderRadius: 2,
+                        bgcolor: 'background.paper',
+                        width: '100%'
+                    }}
+                >
+                    <Typography variant="h4" gutterBottom sx={{ mb: 4, fontWeight: 'bold', color: 'text.primary' }}>
+                        Sign In
+                    </Typography>
+                    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                        <SignIn redirectUrl="/flashcards" />
+                    </Box>
+                </Paper>
+            </Container>
+        </Box>
     );
 }
--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -1,42 +1,47 @@
-import { AppBar, Container, Button, Typography, Toolbar } from "@mui/material";
+'use client'
+
+import { AppBar, Container, Button, Typography, Toolbar, Paper, useTheme, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
 import { SignUp } from '@clerk/nextjs';
 import Link from 'next/link';
 
 export default function SignUpPage() {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <Container maxWidth="lg">
-            
+        <>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        Sign Up
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Flashcard SaaS
                     </Typography>
-                   
                     <Button color="inherit" component={Link} href="/">
                         Home
                     </Button>
                 </Toolbar>
+
             </AppBar>
-            <Typography variant="body2" sx={{ marginTop: 2 }} align="center" alignItems={'center'}>
-                    Already have an account?{' '}
-                    <Link href="/sign-in" passHref>
-                        <Button variant="text">Sign In</Button>
-                    </Link>
-                </Typography>
-            <br />
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    minHeight: '80vh',
-                }}
-            >
-                <SignUp />
-                
-            </Box>
-        </Container>
+            <br></br>
+            <Typography variant="body2" align="center">
+                                Already have an account?{' '}
+                                <Link href="/sign-in">
+                                    Sign In
+                                </Link>
+                            </Typography>
+                     
+            <Container maxWidth="sm">
+                <Box sx={{ mt: 8, mb: 4 }}>
+                    <Paper elevation={3} sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Typography component="h1" variant="h5" gutterBottom>
+                            Sign Up
+                        </Typography>
+                        <SignUp />
+                        
+                            
+                    </Paper>
+                </Box>
+            </Container>
+        </>
     );
 }
